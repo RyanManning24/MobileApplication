420fddda5dc61803aab771efbc7fedd0477bd8ca
Ryan <ryan2.manning@live.uwe.ac.uk>
2021-05-07 20:04:55 +0100

    Recycle View added

--- GameReminder2/app/src/main/java/com/example/gamereminder/MainActivity.kt
+++ GameReminder2/app/src/main/java/com/example/gamereminder/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.gamereminder
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var titlesList = mutableListOf<String>()
+    private var descList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rv_recyclerView: RecyclerView = findViewById(R.id.rv_recyclerView)
+
+        postToList()
+
+        rv_recyclerView.layoutManager = LinearLayoutManager(this)
+        rv_recyclerView.adapter = RecyclerAdapter(titlesList, descList, imagesList)
+    }
+    private fun addToList(title: String, description: String, image: Int){
+        titlesList.add(title)
+        descList.add(description)
+        imagesList.add(image)
+    }
+
+    private fun postToList(){
+        for(i :Int in 1..25){
+            addToList("Title $i", "Description $i", R.mipmap.ic_launcher_round)
+        }
     }
 }
