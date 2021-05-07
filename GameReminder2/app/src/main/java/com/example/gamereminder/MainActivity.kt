50ec8c6b3db54a964d1decd946846c42bead44d8
Ryan <ryan2.manning@live.uwe.ac.uk>
2021-05-07 13:11:56 +0100

    Added Search

--- GameReminder2/app/src/main/java/com/example/gamereminder/MainActivity.kt
+++ GameReminder2/app/src/main/java/com/example/gamereminder/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.gamereminder
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ArrayAdapter <*>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lv_listView: ListView = findViewById(R.id.lv_listview)
+        val tv_textView: TextView = findViewById(R.id.tv_emptyTextView)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, resources.getStringArray((R.array.gamenames_array)))
+
+        lv_listView.adapter = adapter
+        lv_listView.onItemClickListener = AdapterView.OnItemClickListener{parent, view, position, id ->
+            Toast.makeText(applicationContext, parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+        }
+        lv_listView.emptyView = tv_textView
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu,menu)
+
+        val search: MenuItem? = menu?.findItem(R.id.nav_search)
+        val searchView: SearchView = search?.actionView as SearchView
+        searchView.queryHint = "Search For A Game"
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+
 }
+
