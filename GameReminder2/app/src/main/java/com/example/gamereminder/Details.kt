5f0e4f0a0a9e1921cf5aec242b0c6b0037efcc95
Ryan <ryan2.manning@live.uwe.ac.uk>
2021-05-09 00:53:19 +0100

    Need to add details page functionality

--- GameReminder2/app/src/main/java/com/example/gamereminder/Details.kt
+++ GameReminder2/app/src/main/java/com/example/gamereminder/Details.kt
@@ -7,5 +7,21 @@ class Details : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        //get id and create view
+        when(intent.getIntExtra("ID", 0)) {
+            0 -> println("Ratchet And Clan")
+            1 -> println("DeathLoop")
+            2 -> println("Resident Evil")
+            3 -> println("Mass Effect")
+            4 -> println("Chivalry 2")
+            5 -> println("Operation Tango")
+            6 -> println("Mario Golf")
+            7 -> println("Days Gone(PC)")
+
+
+        }
     }
+
+
 }
